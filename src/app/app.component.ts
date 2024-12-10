ed1da1da2ba9674e24455c325c3bc79e65b2e24e
nickhildpac <162016801+nickhildpac@users.noreply.github.com>
2024-12-10 17:44:45 +0530

    standalone configurable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,26 @@ import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-user';
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [TasksComponent, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
 
-  selectedUser(id: string) {
-    console.log('selected id is ' + id);
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    // console.log('selected id is ' + id);
+    this.selectedUserId = id;
   }
 
 }
